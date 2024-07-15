07566f9011fd3f17af656e308ab7c01a79049921
최민수 <study.todair@gmail.com>
2024-07-15 17:05:54 +0900

    java 인터페이스

---
+++ workspace_java/_chap08/src/sec01/exam01/SmartTv.java
@@ -0,0 +1,29 @@
+package sec01.exam01;
+
+public class SmartTv implements RemoteControl, Searchable, Ticket {
+
+	@Override
+	public void search(String keyword) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setVolume(int volume) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
